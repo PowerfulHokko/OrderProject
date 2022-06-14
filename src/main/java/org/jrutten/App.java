9a2f73804958671152e62a6adbbc3a937ffa3a4f
Jurgen Rutten <jurgen.rutten@gmx.com>
2022-06-14 10:47:10 +0200

    init

---
+++ src/main/java/org/jrutten/App.java
@@ -0,0 +1,13 @@
+package org.jrutten;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
