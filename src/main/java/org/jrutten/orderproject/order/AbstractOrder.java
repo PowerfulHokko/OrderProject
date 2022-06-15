4451280cb3d6f02122acd175e027b992526c9eee
Jurgen Rutten <jurgen.rutten@gmx.com>
2022-06-15 10:28:34 +0200

    story 2

--- src/main/java/org/jrutten/orderproject/order/AbstractOrder.java
+++ src/main/java/org/jrutten/orderproject/order/AbstractOrder.java
@@ -8,16 +8,16 @@ import java.util.List;
 import java.util.UUID;
 
 public abstract class AbstractOrder {
-    private final String customerId;
+//    private final String customerId;
     private String orderID;
     private List<OrderedItem> orderedItemsList;
     private Date shippindDate;
     private double totalPrice;
 
     AbstractOrder(String customerId, List<OrderedItem> items){
-        this.customerId = customerId;
+//        this.customerId = customerId;
         this.orderID = UUID.randomUUID().toString();
-        this.orderedItemsList =
+//        this.orderedItemsList =
     }
 
 }
