503d4484df8e63ddcd65c563ddf42f4799bf30e6
Jurgen Rutten <jurgen.rutten@gmx.com>
2022-06-15 10:21:12 +0200

    story 2

--- src/main/java/org/jrutten/orderproject/order/AbstractOrder.java
+++ src/main/java/org/jrutten/orderproject/order/AbstractOrder.java
@@ -5,51 +5,20 @@ import org.springframework.core.annotation.Order;
 
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 public abstract class AbstractOrder {
+    private final String customerId;
     private String orderID;
     private List<OrderedItem> orderedItemsList;
     private Date shippindDate;
     private double totalPrice;
 
+    AbstractOrder(String customerId, List<OrderedItem> items){
+        this.customerId = customerId;
+        this.orderID = UUID.randomUUID().toString();
+        this.orderedItemsList =
+    }
 
 }
 
-class OrderedItem{
-    private final Item item;
-    private final String itemId;
-    private final String itemName;
-    private final double pricePerPieceAtOrderTime;
-    private int amount;
-
-    OrderedItem(Item item, int amount){
-        this.item = item;
-        this.amount = amount;
-
-        double temp = item.getPrice();
-        this.pricePerPieceAtOrderTime = temp;
-
-        this.itemId = item.getItemId();
-        this.itemName = item.getName();
-    }
-
-    public String getItemId() {
-        return itemId;
-    }
-
-    public String getItemName() {
-        return itemName;
-    }
-
-    public double getPricePerPieceAtOrderTime() {
-        return pricePerPieceAtOrderTime;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-}
